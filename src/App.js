b6f8f7c8c6767b453e5c4ce4523fcfb96c5fd86d
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-21 09:20:02 +0530

    Implement BookForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BookContextProvider>
         <Navbar />
         <BookList />
+        <BookForm />
       </BookContextProvider>
     </div>
   );
